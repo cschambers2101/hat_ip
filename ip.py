145f266a714a550c5164c47180c16b920aca4283
Craig Chambers <cchambers@salisbury6c.ac.uk>
2016-03-03 12:39:57 +0000

    Added showing IP address

--- ip.py
+++ ip.py
@@ -1,8 +1,10 @@
 # Gets IP address of Raspberry Pi and displays it on the Sense Hat LEDs
 # Run at boot to show IP address to SSH into
 
-import sense_hat from SenseHat
+from sense_hat import SenseHat
 from time import sleep
 from subprocess import *
 
 ip = Popen("ip addr show wlan0 | grep inet | awk '{print $2}' |cut -d -f1", shell=True, stdout=PIPE).communicate()[0]
+
+sense.show_message(ip)
